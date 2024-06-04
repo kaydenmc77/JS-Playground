9eed4b187f6a1214df849fb639be5480a43a30e5
kaydenmc77 <kayden-mcmanaway@live.nmit.ac.nz>
2024-06-04 13:40:43 +1200

    4.6.24 Update

--- functions6.js
+++ functions6.js
@@ -4,11 +4,49 @@
 function rotateArray(arr, num) {
     let result = arr
     for(let i = 0; i<num; i++) {
-        let firstNumber = arr.pop() 
-        arr.unshift(firstNumber)
+        let lastNumber = arr.pop() 
+        arr.unshift(lastNumber)
         result = arr
     }
     return result
 }
 
-console.log(rotateArray([1, 2, 3, 4, 5, 6], 4))
+// console.log(rotateArray([1, 2, 3, 4, 5, 6], 4))
+
+//Create a function that takes an array of values, removes all duplicates
+
+//Mine - Doesn't work
+function declutterArray(array) {
+    let result = array
+    for(let i = 0; i<array.length; i++) {
+        let counter = 0;
+        for(let j = 0; j<array.length; j++) {
+            if(array[j] == array[i]) {
+                counter++
+                if(counter >= 2) {
+                    let theArray = array.slice(j, j)
+
+                    console.log(array)
+                }
+            }
+        }
+    }
+    return result
+}
+
+console.log(declutterArray([1, 2, 3, 3, 6, 6, 8]))
+
+//Isaac
+function cleanArray(arr) {
+    let repeats = []
+        arr.forEach(value => {
+            if(!repeats.includes(value)) {
+                repeats.push(value)
+            }
+        });
+        return repeats
+    }
+
+console.log(cleanArray([1,1,4,3,5,'dog','cat','dog']))
+
+//Create a function that 
